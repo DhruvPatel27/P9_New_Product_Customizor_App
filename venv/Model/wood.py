068f29c1d1bc3c4d63024d28d00781659932cf90
unknown <pchugh1@asu.edu>
2020-03-10 18:40:21 -0700

    Fetching the woodmasks by id

--- venv/Model/wood.py
+++ venv/Model/wood.py
@@ -1,13 +1,13 @@
 import Model.db_connection as db_connection
 
 #API to get all wood images from the database
-def get_wood():
+def get_wood(wood_id):
     connection = db_connection.get_connection()
     try:
         with connection.cursor() as cursor:
-            sql = "SELECT * from WOOD_TYPE"
-            cursor.execute(sql)
-            result = cursor.fetchall()
+            sql = "SELECT * from WOOD_TYPE where `ID`=%s"
+            cursor.execute(sql,wood_id)
+            result = cursor.fetchone()
     finally:
         connection.close()
         cursor.close()
