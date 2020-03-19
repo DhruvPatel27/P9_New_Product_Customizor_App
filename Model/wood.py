b9d58598eb36af7c5a699a91f50917f10ec7a7c0
Dhruv Patel <dhruvpatel0127@gmail.com>
2020-03-19 12:00:53 -0700

    Wood images are now fetched from db

--- Model/wood.py
+++ Model/wood.py
@@ -1,13 +1,29 @@
 import Model.db_connection as db_connection
 
-#API to get all wood images from the database
-def get_wood(wood_id):
+
+# API to get wood images by id from the database
+def get_wood_by_id(wood_id):
     connection = db_connection.get_connection()
     try:
         with connection.cursor() as cursor:
             sql = "SELECT * from WOOD_TYPE where `ID`=%s"
-            cursor.execute(sql,wood_id)
+            cursor.execute(sql, wood_id)
             result = cursor.fetchone()
+    finally:
+        connection.close()
+        cursor.close()
+
+    return result
+
+
+# API to get all wood images from the database
+def get_wood():
+    connection = db_connection.get_connection()
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * from WOOD_TYPE"
+            cursor.execute(sql)
+            result = cursor.fetchall()
     finally:
         connection.close()
         cursor.close()
--- application.py
+++ application.py
@@ -12,7 +12,8 @@ application = Flask(__name__)
 def get_product_by_id():
     id_name = request.args.get('id')
     result = product.get_product_details(id_name)
-    return render_template('product-details.html', product=result, len=len(result))
+    wood_type = wood.get_wood()
+    return render_template('product-details.html', product=result, len=len(result), wood_type=wood_type)
 
 @application.route('/products/all', methods=['GET'])
 def get_products():
@@ -116,7 +117,7 @@ def render_logout():
     url=""
     result = product.get_products()
     total_pages = (len(result) % 12) + 1
-    return render_template('product-catalog.html', product=result, len=len(result), url=url, total_pages=total_pages),200
+    return render_template('product-catalog.html', product=result, len=len(result), url=url, total_pages=total_pages), 200
 
 @application.route('/Occasion1.html')
 def render_occasion():
@@ -131,7 +132,7 @@ def show_preview():
     model_id = request.args.get('model_id')
     wood_id = request.args.get('wood_id')
     mask = product.get_products_mask(model_id)
-    wood_type = wood.get_wood(wood_id)
+    wood_type = wood.get_wood_by_id(wood_id)
 
     preview_image = preview.mask_loop(mask[0]['model_mask'], wood_type['image'])
 
