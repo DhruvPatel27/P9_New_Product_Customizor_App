789618c17832409d0f1a0fff8465dc4fa71a96a5
Trupti Khatavkar <khatavkar.trupti@gmail.com>
2020-04-02 07:19:02 -0700

    US #158 Task #188 Fetch woodtype_id and woodpattern_id from product details into session

--- application.py
+++ application.py
@@ -167,25 +167,22 @@ def remove_from_cart_page():
     id_name = request.args.get('id')
     user_name = session['user_name']
     order.remove_cart(user_name, id_name)
-    return render_template("success.html")
-    # if 'product' in session:
-    #     data = session['product']
-    #     for i in range(len(data)):
-    #         if id_name in data[i]:
-    #             del data[i]
-    #             break
-    #     session['product'] = data
-    #     product_result = []
-    #     for dic in data:
-    #         for prod_id, q in dic.items():
-    #             result = product.get_product_details_cart(prod_id)
-    #             res_img = result['image']
-    #             result['image'] = res_img.decode('utf-8')
-    #             result['quantity'] = q[0]
-    #             product_result.append(result)
-    #     return jsonify({
-    #         "product": product_result
-    #         })
+    result = order.load_cart(user_name)
+    product_result = []
+    for entry in result:
+        prod = product.get_product_details_cart(entry['product_id'])
+        entry['title'] = prod['title']
+        entry['price'] = prod['price']
+        res_img = entry['image']
+        entry['image'] = res_img.decode('utf-8')
+        wood_type = wood.get_wood_by_id(entry['woodtype_id'])
+        entry['wood_type'] = wood_type['name']
+        design_type = wood.get_design_by_id(entry['woodpattern_id'])
+        entry['wood_pattern'] = design_type['name']
+        product_result.append(entry)
+    return jsonify({
+        "product": product_result
+    })
 
 
 @application.route('/add-to-cart', methods=['POST'])
@@ -196,25 +193,25 @@ def add_to_cart():
     quantity = request.form['quantity']
     wood_id = request.form['wood']
     pattern_id = request.form['pattern']
+    print(pattern_id)
     user_name = session['user_name']
     result = product.get_product_details_cart(id_name)
     price = result['price']
     total_cost = price * float(quantity)
     cart_details = order.add_to_cart(id_name, image, quantity, wood_id, pattern_id, user_name, total_cost)
-    if (cart_details):
-        url = ""
-        result = product.get_products()
+    url = ""
+    result = product.get_products()
 
-        total_pages = (int)(len(result) / 12) + 1
+    total_pages = (int)(len(result) / 12) + 1
 
-        if (page == None or int(page) == 1):
-            result = list(itertools.islice(result, 0, 12, 1))
-            return render_template('product-catalog.html', product=result, len=len(result), url=url,
+    if (page == None or int(page) == 1):
+        result = list(itertools.islice(result, 0, 12, 1))
+        return render_template('product-catalog.html', product=result, len=len(result), url=url,
                                    total_pages=total_pages), 200
-        else:
-            page = int(page)
-            result = result[12 * (page - 1):12 * page]
-            return render_template('product-catalog.html', product=result, len=len(result), url=url,
+    else:
+        page = int(page)
+        result = result[12 * (page - 1):12 * page]
+        return render_template('product-catalog.html', product=result, len=len(result), url=url,
                                    total_pages=total_pages), 200
 
 
