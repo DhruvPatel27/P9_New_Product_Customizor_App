1ceb0ab957bffc6fb14fd56348b8a01a963a7d04
Dhruv Patel <dhruvpatel0127@gmail.com>
2020-04-05 22:21:37 -0700

    Show preview on selecting wood and design type

--- model/customization.py
+++ model/customization.py
@@ -3,15 +3,20 @@ from io import BytesIO
 
 from PIL import Image
 
-def mask_loop(mask, wood_type, design_type):
-    mask = Image.open(BytesIO(base64.b64decode(mask.decode('utf-8'))))
+import model.product as product
+import model.wood as wood
+
+
+def wood_preview(model_id, wood_id):
+    mask = product.get_products_mask(model_id)
+    mask = Image.open(BytesIO(base64.b64decode(mask[0]['model_mask'].decode('utf-8'))))
     mask = mask.convert("RGBA")
     width, height = mask.size
-    wood = Image.open(BytesIO(base64.b64decode(wood_type.decode('utf-8'))))
-    wood = wood.convert("RGBA")
-    design = Image.open(BytesIO(base64.b64decode(design_type.decode('utf-8'))))
-    design = design.convert("RGBA")
-    files = [wood, design, mask]
+
+    wood_type = wood.get_wood_by_id(wood_id)
+    wood_type = Image.open(BytesIO(base64.b64decode(wood_type['image'].decode('utf-8'))))
+    wood_type = wood_type.convert("RGBA")
+    files = [wood_type, mask]
 
     result = Image.new("RGBA", (width, height))
 
@@ -25,4 +30,29 @@ def mask_loop(mask, wood_type, design_type):
     return result_encoded
 
 
+def design_preview(model_id, wood_id, design_id):
+    mask = product.get_products_mask(model_id)
+    mask = Image.open(BytesIO(base64.b64decode(mask[0]['model_mask'].decode('utf-8'))))
+    mask = mask.convert("RGBA")
+    width, height = mask.size
 
+    wood_type = wood.get_wood_by_id(wood_id)
+    wood_type = Image.open(BytesIO(base64.b64decode(wood_type['image'].decode('utf-8'))))
+    wood_type = wood_type.convert("RGBA")
+
+    design_type = wood.get_design_by_id(design_id)
+    design_type = Image.open(BytesIO(base64.b64decode(design_type['mask'].decode('utf-8'))))
+    design_type = design_type.convert("RGBA")
+
+    files = [wood_type, design_type, mask]
+
+    result = Image.new("RGBA", (width, height))
+
+    for i in range(0, len(files)):
+        result.paste(files[i], (0, 0), files[i])
+
+    img = BytesIO()
+    result.save(img, format='PNG')
+    img = img.getvalue()
+    result_encoded = base64.b64encode(img)
+    return result_encoded
