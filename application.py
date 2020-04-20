862e488fe6438029e23eed921455876649c62b67
Dhruv Patel <dhruvpatel0127@gmail.com>
2020-04-19 18:56:41 -0700

    Added background on message

--- application.py
+++ application.py
@@ -1,8 +1,7 @@
 import itertools
 
-
-import pandas as pd
 import numpy as np
+import pandas as pd
 from flask import Flask, render_template, request, session, jsonify, redirect
 
 import model.customization as preview
@@ -25,20 +24,22 @@ def get_product_by_id():
     return render_template('product-details.html', product=result, len=len(result), wood_type=wood_type,
                            wood_design=wood_design, default_image=default_image)
 
+
 @application.route('/')
 def render_static():
     page = request.args.get('page')
     result = product.get_products()
     details = get_pages(page, result)
     return render_template('product-catalog.html', product=details[0], len=len(details[0]), url="",
-                               total_pages=details[1])
+                           total_pages=details[1])
+
 
 @application.route('/login', methods=['POST'])
 def login():
     if 'customer' in session and session['customer']:
         return redirect(request.url_root)
     elif 'manager' in session and session['manager']:
-        return redirect(request.url_root+"manager")
+        return redirect(request.url_root + "manager")
     else:
         session.clear()
     page = request.args.get('page')
@@ -56,12 +57,13 @@ def login():
             return render_template('woodworker.html', orders=orders, len=len(orders), url=url), 200
         elif user_details['Role'] == "Admin":
             session['manager'] = True
-            return redirect(request.url_root+"manager")
+            return redirect(request.url_root + "manager")
         else:
             session['customer'] = True
             return redirect(request.url_root)
     else:
         return render_template('login.html', error="Invalid credentials. Try again!!!"), 401
+
 
 @application.route('/manager', methods=['GET'])
 def display_home_manager():
@@ -70,9 +72,10 @@ def display_home_manager():
         result = product.get_products()
         details = get_pages(page, result)
         return render_template('product-catalog-manager.html', product=details[0], len=len(details[0]), url="",
-                               total_pages=details[1]),200
+                               total_pages=details[1]), 200
     else:
         return render_template('error.html', error="You are not authorized to access this page!!"), 401
+
 
 @application.route('/signup', methods=['POST'])
 def signup():
@@ -81,8 +84,9 @@ def signup():
         return render_template('signup.html'), 400
     else:
         user.signup(request.form['lastname'], request.form['firstname'], request.form['emailid'],
-                                   request.form['password'])
+                    request.form['password'])
         return redirect(request.url_root)
+
 
 @application.route('/account', methods=['GET'])
 def get_user_by_id():
@@ -190,6 +194,7 @@ def render_about_us():
 def render_basic_layout():
     return render_template('basic-layout.html')
 
+
 @application.route('/basic-layout-manager.html')
 def render_basic_layout_manager():
     return render_template('basic-layout-manager.html')
@@ -200,12 +205,14 @@ def manage_products():
     if request.method == 'POST':
         new_products = request.files['new-products']
         data_xls_1 = pd.read_excel(new_products)
-        data_xls = data_xls_1.replace(np.nan,'',regex=True)
+        data_xls = data_xls_1.replace(np.nan, '', regex=True)
         invalid = product.add_products(data_xls)
         if not invalid:
             return render_template('manager-success.html', success="Products added successfully"), 200
         else:
-            return render_template('manager-success.html', success="Some products are added successfully. Please correct the details for the following products.", error=invalid), 200
+            return render_template('manager-success.html',
+                                   success="Some products are added successfully. Please correct the details for the following products.",
+                                   error=invalid), 200
 
     return render_template('manage-products.html'), 200
 
@@ -220,6 +227,7 @@ def render_logout():
     session.clear()
     return redirect(request.url_root)
 
+
 @application.route('/occasions', methods=['GET'])
 def render_occasion():
     occasion = str(request.args.get('occasion'))
@@ -230,7 +238,8 @@ def render_occasion():
         result = product.get_products_by_occasion(occasion)
     details = get_pages(page, result)
     return render_template('product-catalog.html', product=details[0], len=len(details[0]), url="",
-                               total_pages=details[1])
+                           total_pages=details[1])
+
 
 @application.route('/categories', methods=['GET'])
 def render_category():
@@ -243,7 +252,8 @@ def render_category():
 
     details = get_pages(page, result)
     return render_template('product-catalog.html', product=details[0], len=len(details[0]), url="",
-                               total_pages=details[1])
+                           total_pages=details[1])
+
 
 @application.route('/orderstatus', methods=['GET'])
 def show_order_status():
@@ -252,8 +262,10 @@ def show_order_status():
     result = order.get_order_details_by_id(order_id)
     wood_type = wood.get_wood_by_id(result['woodtype_id'])
     wood_design = wood.get_design_by_id(result['woodpattern_id'])
-    products=product.get_product_details(result['product_id'])
-    return render_template('order-status.html', url=url, orders=result,len=len(result), product=products, wood_type=wood_type, wood_design=wood_design),200
+    products = product.get_product_details(result['product_id'])
+    return render_template('order-status.html', url=url, orders=result, len=len(result), product=products,
+                           wood_type=wood_type, wood_design=wood_design), 200
+
 
 @application.route('/orderstatus/update', methods=['GET'])
 def update_order_status():
@@ -266,7 +278,7 @@ def update_order_status():
 
 
 @application.route('/preview', methods=['GET'])
-def show_message_preview():
+def show_preview():
     model_id = request.args.get('model_id')
     wood_id = request.args.get('wood_id')
     design_id = request.args.get('design_id')
@@ -298,7 +310,8 @@ def edit_product():
         description = request.form['description']
         price = request.form['price']
         product.edit(p_id, title, description, price)
-        return render_template('manager-success.html', success="Product Updated"),200
+        return render_template('manager-success.html', success="Product Updated"), 200
+
 
 # Returns results based on pages
 def get_pages(page, result):
@@ -308,11 +321,11 @@ def get_pages(page, result):
     else:
         page = int(page)
         result = result[12 * (page - 1):12 * page]
-    
-    return (result,total_pages)
+
+    return (result, total_pages)
+
 
 if __name__ == '__main__':
     application.secret_key = 'super secret key'
     application.debug = True
     application.run()
-   
