e072dffa795f489848d1ed759a275fcd2c363b93
Lehar Bhatt <bhattlehar@gmail.com>
2020-04-22 16:51:46 -0700

    Add test cases to product file

--- test/product_test.py
+++ test/product_test.py
@@ -1,13 +1,22 @@
 import unittest
 from unittest.mock import patch
-from model.product import get_products
+import model.product as product
 
 class ProductTestCase(unittest.TestCase):
 
-    @patch('model.product.db_connection')
-    def test_get_products(self,mock_conn):
-        r = get_products()
-        mock_conn.get_connection.assert_called_once()
+    def test_get_products(self):
+        test_products = product.get_products()
+        self.assertEqual(len(test_products), 4)
+    
+    def test_get_product_details_correct_id(self):
+        test_products = product.get_product_details(1)
+        self.assertEqual(test_products['title'], 'Engraved Wood Coaster')
+
+    def test_get_product_details_incorrect_id(self):
+        test_products = product.get_product_details(20)
+        self.assertEqual(test_products, None)
+    
+
 
 if __name__ == '__main__':
     unittest.main()
